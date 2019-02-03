38b30856c7392e0e9422e680e83e2fbb19eb1cfc
Anna <annabaumann@laposte.net>
2019-02-03 18:14:40 +0100

    ahh gradient grid

--- sketch.js
+++ sketch.js
@@ -30,6 +30,7 @@ let gapX;
 let gapY;
 let startTime;
 let bgColor;
+let pointColor;
 
 function initArray(){
   for(let i = 0 ; i < numberPointX; i++ ){
@@ -44,34 +45,55 @@ function initArray(){
 function setup() {
   createCanvas(400, 400);
   background(255);
-  // stroke(0, 15);
-  // noFill();
-  // stroke(0);
-  noStroke();
-  fill(0, 0, 0, 105);
-  // t = 0;
-  // xPosition = lngToXWorld(long, projectionSize)
-  // yPosition = latToYWorld(long, projectionSize)
-  bgColor = 255;
-  numberPointX = 10;
-  numberPointY = 10;
+  stroke(0);
+  bgColor = 'rgba(0, 0, 0, 1)';
+  pointColor = 255;
+  numberPointX = 20;
+  numberPointY = 20;
   pointArray = [];
   gapX = width / numberPointX -1;
   gapY = height / numberPointY - 1;
   initArray();
   startTime = Date.now();
+  background(bgColor);
+
+}
+
+function drawGrid(millis){
+  from = color('rgba(255, 0, 30, 0.2)');
+  to = color('rgba(0, 0, 255, 0.2)');
+  for(let i = 0; i <pointArray.length ; i ++){
+
+    x = pointArray[i][0];
+    c1 = lerpColor(from, to, (1/pointArray.length)*i, 0.2);
+    x = 50 * noise(x * 0.01, pointArray[i][1] * millis)+pointArray[i][0];
+    y = 50 * noise(x * 0.01, pointArray[i][1] * millis)+pointArray[i][1];
+    fill(c1);
+    ellipse(x, y, 5, 5);
+    // point(x,y);
+    // rect(x, y, 70, 70);
+  }
+}
+
+function drawMyPosition(x, y, millis){
+  fill('rgba(255, 255, 255, 0.1)');
+  // colorMode(HSB, 360, 100, 100);
+  blendMode(DIFFERENCE);
+  let index = x*numberPointY + y;
+  let realX = pointArray[index][0];
+  let realY = pointArray[index][1];
+  console.log(Math.abs(cos(millis*10)));
+  ellipse(realX, realY, Math.abs(cos(millis*1000))*10, Math.abs(cos(millis*1000)*10));
+  blendMode(NORMAL);
 }
 
 function draw() {
-  background(bgColor);
   let millis = (Date.now() - startTime)/1000000;
-  for(let i = 0; i <pointArray.length ; i ++){
-      x = pointArray[i][0];
-      x = 50 * noise(x * 0.01, pointArray[i][1] * millis)+pointArray[i][0];
-      y = 50 * noise(x * 0.01, pointArray[i][1] * millis)+pointArray[i][1];
-      rect(x, y, 70, 70);
-  }
-
+  noStroke();
+  // blendMode(SCREEN);
+  fill(pointColor);
+  drawGrid(millis);
+  drawMyPosition(3, numberPointY-4, millis);
   // }
   // commencer le tracer
   // beginShape();
