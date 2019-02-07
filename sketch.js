d15f7cf576a2b6a94933d0779a583b0874ffe926
Anna <annabaumann@laposte.net>
2019-02-07 15:19:36 +0100

    right

--- sketch.js
+++ sketch.js
@@ -197,7 +197,7 @@ function draw() {
   fill(255);
   textSize(16);
   push();
-  translate(width*0.5, 0);
+  translate(width, 0);
   text(textLongPos, 10 , height-height*0.1);
   text(textLatPos, 10 , height-height*0.1-26);
   pop();
